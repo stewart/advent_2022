c3b71b71a1292d6f5f7728bd3a0b06eed3fe9917
Andrew Stewart <andrew@stwrt.ca>
2022-12-05 22:08:14 -0800

    Day 06
    This could probably be more cleverer but I'm not too worried about it for now.

--- lib/advent/day_06.ex
+++ lib/advent/day_06.ex
@@ -13,14 +13,29 @@ defmodule Advent.Day06 do
   @impl Advent
   def input do
     @input_file
-    |> split()
+    |> trim()
+    |> to_charlist()
   end
 
   def part_one(data) do
+    start_of_packet? = fn {buffer, offset} ->
+      if buffer == uniq(buffer), do: offset
+    end
+
     data
+    |> chunk_every(4, 1)
+    |> with_index(4)
+    |> find_value(start_of_packet?)
   end
 
   def part_two(data) do
+    start_of_message? = fn {buffer, offset} ->
+      if buffer == uniq(buffer), do: offset
+    end
+
     data
+    |> chunk_every(14, 1)
+    |> with_index(14)
+    |> find_value(start_of_message?)
   end
 end
